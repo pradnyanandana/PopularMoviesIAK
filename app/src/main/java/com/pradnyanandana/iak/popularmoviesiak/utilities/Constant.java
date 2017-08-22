0ff12fd43100cdbceeadfbbeb7b5dc1b256e48c3
pradnyanandana <pradnyanandana@gmail.com>
2017-08-23 00:04:46 +0800

    Membuat class Constant

---
+++ app/src/main/java/com/pradnyanandana/iak/popularmoviesiak/utilities/Constant.java
@@ -0,0 +1,15 @@
+package com.pradnyanandana.iak.popularmoviesiak.utilities;
+
+import com.pradnyanandana.iak.popularmoviesiak.BuildConfig;
+
+/**
+ * Created by pradn on 23/08/2017.
+ */
+
+public class Constant {
+    public static final String URL_API_MOVIE = "https://api.themoviedb.org/3/movie/";
+    public static final String POPULAR = "popular";
+    public static final String TOP_RATED = "top_rated";
+    public static final String PARAM_API_KEY = "?api_key=";
+    public static final String API_KEY = BuildConfig.API_KEY;
+}
